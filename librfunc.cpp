10ca9aaf98313fd6ab87c508554dc60edaedb233
Nikhil Behera <nb240805@gmail.com>
2024-08-21 23:47:37 +0530

    Add files via upload

---
+++ librfunc.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    int a;
+    cout<<"a:";
+    cin>>a;
+    int b;
+    cout<<"b:";
+    cin>>b;
+    cout<<"The ans is : "<<pow(a,b)<<endl;
+    cout<<"The square root of a is : "<<sqrt(a)<<endl;
+    cout<<"The square root of b is : "<<sqrt(b)<<endl;
+    return 0;
+}
